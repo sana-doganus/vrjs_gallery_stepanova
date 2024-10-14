bb60e840fd85007682fe4f68053bccc989c5c0af
Alexandra Stepanova <128775364+sana-doganus@users.noreply.github.com>
2024-10-14 12:08:14 +0500

    Update info-panel.js

--- info-panel.js
+++ info-panel.js
@@ -9,19 +9,19 @@ AFRAME.registerComponent('info-panel', {
 
     this.movieInfo = {
       karigurashiButton: {
-        title: 'The Secret World of Arrietty (2010)',
+        title: 'Fire^2 (2021)',
         imgEl: document.querySelector('#karigurashiMovieImage'),
         description: 'Based on the 1952 novel The Borrowers by Mary Norton, an English author of children\'s books, about a family of tiny people who live secretly in the walls and floors of a typical household, borrowing items from humans to survive.'
       },
       kazetachinuButton: {
-        title: 'The Wind Rises (2013)',
+        title: 'The Cycle of Life and Other Meaningful BS (2020)',
         imgEl: document.querySelector('#kazetachinuMovieImage'),
-        description: 'The Wind Rises is a fictionalised biographical film of Jiro Horikoshi (1903, 1982), designer of the Mitsubishi A5M fighter aircraft and its successor, the Mitsubishi A6M Zero, used by the Empire of Japan during World War II. The film is adapted from Miyazaki\'s manga of the same name, which was in turn loosely based on both the 1937 novel The Wind Has Risen by Tatsuo Hori and the life of Jiro Horikoshi.'
+        description: 'AAA'
       },
       ponyoButton: {
-        title: 'Ponyo (2003)',
+        title: 'Satellite (2022)',
         imgEl: document.querySelector('#ponyoMovieImage'),
-        description: 'It is the eighth film Miyazaki directed for Studio Ghibli, and his tenth overall. The film tells the story of Ponyo (Nara), a goldfish who escapes from the ocean and is rescued by a five-year-old human boy, Sōsuke (Doi) after she is washed ashore while trapped in a glass jar.'
+        description: 'I'
       }
     };
 
